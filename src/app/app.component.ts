fe36ffd9367c884049b1c6f3a7a81c4726316cec
Siphumelelise Ntshwenyese <sjayjay005@gmail.com>
2017-09-21 15:32:37 +0200

    JUBV1-003 Third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{taxi.name}} details!</h2>
+    <div><label>id: </label>{{taxi.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="taxi.name" placeholder="name">
+    </div>
+    `
 })
+
+// const TAXIS: Taxi[] = [
+//   {id: 11, name: 'Khanyisa\'s taxi'},
+//   {id: 12, name: 'Russel\'s taxi'},
+//   {id: 13, name: 'Gershwin\'s taxi'},
+//   {id: 14, name: 'Jayjay\'s taxi'},
+//   {id: 15, name: 'Wanga\'s taxi'},
+//   {id: 16, name: 'Wessley\'s taxi'},
+//   {id: 17, name: 'Ryan\'s taxi'},
+//   {id: 18, name: 'Elzanne\'s taxi'},
+//   {id: 19, name: 'Matthew\'s taxi'},
+//   {id: 20, name: 'Brendo\'s taxi'},
+//   {id: 21, name: 'Cammiel\'s taxi'},
+//   {id: 22, name: 'Kate\'s taxi'},
+// ];
+
 export class AppComponent {
-  title = 'Welcome to J-Uber.';
+  title = 'Welcome to J\'Uber';
+  taxi: Taxi = {id: 1 , name: 'Matabisi\'s taxi'};
+  taxis = TAXIS;
+}
+
+export class Taxi {
+  id: number;
+  name: string;
 }
